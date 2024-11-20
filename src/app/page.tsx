8754b6f3d0bf708ca34dc66caddd313ffb520b38
rockske8er <rockske8er@gmail.com>
2024-11-20 11:22:32 -0300

    feat: create statistics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,100 @@
-import { TimeCard } from '@/components/time-card'
+import { TimeCardFull } from '@/components/time-card-full'
 import Image from 'next/image'
 
+const data = [
+  {
+    time1: {
+      name: 'Revoada',
+      shield: '/images/asset-1.webp',
+    },
+    time2: {
+      name: 'Barça 11',
+      shield: '/images/asset-2.webp',
+    },
+    rodada: 'Primeira Rodada',
+    date: 'Sábado, 11/23/2024',
+    time: '19:00',
+    stadium: 'Quadra Poliesportiva AG 11',
+  },
+
+  {
+    time1: {
+      name: 'Os Galácticos',
+      shield: '/images/asset-1.webp',
+    },
+    time2: {
+      name: 'Morro da Providência',
+      shield: '/images/asset-2.webp',
+    },
+    rodada: 'Primeira Rodada',
+    date: 'Sábado, 11/23/2024',
+    time: '20:00',
+    stadium: 'Quadra Poliesportiva AG 11',
+  },
+
+  {
+    time1: {
+      name: 'Real Madruga',
+      shield: '/images/asset-1.webp',
+    },
+    time2: {
+      name: 'Velho Oeste',
+      shield: '/images/asset-2.webp',
+    },
+    rodada: 'Primeira Rodada',
+    date: 'Sábado, 11/23/2024',
+    time: '21:00',
+    stadium: 'Quadra Poliesportiva AG 11',
+  },
+
+  {
+    time1: {
+      name: 'PSG Pituba',
+      shield: '/images/asset-1.webp',
+    },
+    time2: {
+      name: 'Doces Souza',
+      shield: '/images/asset-2.webp',
+    },
+    rodada: 'Primeira Rodada',
+    date: 'Terça, 11/26/2024',
+    time: '19:00',
+    stadium: 'Quadra Poliesportiva AG 11',
+  },
+
+  {
+    time1: {
+      name: 'Serrado',
+      shield: '/images/asset-1.webp',
+    },
+    time2: {
+      name: 'Atlético Taquari',
+      shield: '/images/asset-2.webp',
+    },
+    rodada: 'Primeira Rodada',
+    date: 'Terça, 11/26/2024',
+    time: '20:00',
+    stadium: 'Quadra Poliesportiva AG 11',
+  },
+
+  {
+    time1: {
+      name: 'Fênix',
+      shield: '/images/asset-1.webp',
+    },
+    time2: {
+      name: 'Vila Real',
+      shield: '/images/asset-2.webp',
+    },
+    rodada: 'Primeira Rodada',
+    date: 'Terça, 11/26/2024',
+    time: '21:00',
+    stadium: 'Quadra Poliesportiva AG 11',
+  },
+]
 export default function Home() {
   return (
-    <div className="flex w-full py-14 flex-col ">
+    <div className="flex w-full h-screen py-14 flex-col ">
       <div className="w-full  flex items-center  justify-center max-w-7xl mx-auto ">
         <Image
           alt="image"
@@ -14,129 +105,39 @@ export default function Home() {
         />
       </div>
 
-      <div className="w-full flex max-w-7xl mx-auto my-5">
-        <TimeCard image={'/images/asset-1.webp'} name={'Team 1'} />
+      <div className="w-full px-5">
+        <div className="w-full max-w-7xl mx-auto">
+          <header className=" w-full uppercase text-5xl block font-bold text-zinc-100 mb-5 mt-4 ">
+            Proximas Partidas
+          </header>
 
-        <div className="flex  flex-col items-center justify-center">
-          <span className="w-10 font-bold h-10 flex items-center justify-center bg-blue-500 rounded-full">
-            VS
-          </span>
-        </div>
-
-        <TimeCard image={'/images/asset-2.webp'} name={'Team 2'} />
-      </div>
-
-      <div className="w-full flex flex-col items-center justify-center max-w-7xl mx-auto">
-        <header className=" w-full text-2xl block font-bold text-zinc-100 mb-5 text-center ">
-          Placar
-        </header>
-
-        <div className="flex  items-center w-full justify-center">
-          <div className="flex flex-col gap-3 items-center">
-            <TimeCard
-              image={'/images/asset-1.webp'}
-              name={'Team 1'}
-              size={80}
-            />
-            <span className="flex rounded-lg items-center justify-center text-3xl bg-zinc-900 w-12 h-12">
-              0
-            </span>
+          <div className="">
+            <ul className="grid grid-cols-1 md:grid-cols-2 gap-4 items-center">
+              {data.map((item, index) => (
+                <li
+                  key={index}
+                  className="w-full rounded-md bg-zinc-900 flex items-center justify-center  px-4"
+                >
+                  <div className="flex flex-row w-full items-center justify-between">
+                    <TimeCardFull
+                      rodada={item.rodada}
+                      time1={{
+                        name: item.time1.name,
+                        shield: item.time1.shield,
+                      }}
+                      time2={{
+                        name: item.time2.name,
+                        shield: item.time2.shield,
+                      }}
+                      date={item.date}
+                      time={item.time}
+                      stadium={item.stadium}
+                    />
+                  </div>
+                </li>
+              ))}
+            </ul>
           </div>
-
-          <div className="flex flex-col items-center justify-center">
-            <span className="w-6 font-bold  h-6 flex items-center justify-center bg-blue-500 rounded-full">
-              VS
-            </span>
-          </div>
-
-          <div className="flex flex-col gap-3 items-center">
-            <TimeCard
-              image={'/images/asset-2.webp'}
-              name={'Team 2'}
-              size={80}
-            />
-            <span className="flex rounded-lg items-center justify-center text-3xl bg-zinc-900 w-12 h-12">
-              0
-            </span>
-          </div>
-        </div>
-      </div>
-
-      <div className="">
-        <header className=" w-full text-2xl block font-bold text-zinc-100 mb-5 text-center">
-          Estatisticas da Partida
-        </header>
-
-        <div className="w-full flex max-w-7xl mx-auto flex-col">
-          <div className=" flex flex-row">
-            <div className="bg-yellow-300 w-full">
-              <TimeCard image={'/images/asset-1.webp'} />
-            </div>
-            <div className="bg-zinc-900 w-full">
-              <TimeCard image={'/images/asset-2.webp'} />
-            </div>
-          </div>
-
-          <ul className="w-full space-x-4">
-            <li className="flex items-center justify-between border-b border-zinc-700 px-4 py-4">
-              <div className="">
-                <span>-</span>
-              </div>
-              <div className="">
-                <strong>Total de Gols</strong>
-              </div>
-              <div className="">-</div>
-            </li>
-
-            <li className="flex items-center justify-between border-b border-zinc-700 px-4 py-4">
-              <div className="">
-                <span>-</span>
-              </div>
-              <div className="">
-                <strong>Gol</strong>
-              </div>
-              <div className="">-</div>
-            </li>
-
-            <li className="flex items-center justify-between border-b border-zinc-700 px-4 py-4">
-              <div className="">
-                <span>-</span>
-              </div>
-              <div className="">
-                <strong>Total de cartoes</strong>
-              </div>
-              <div className="">-</div>
-            </li>
-            <li className="flex items-center justify-between border-b border-zinc-700 px-4 py-4">
-              <div className="">
-                <span>-</span>
-              </div>
-              <div className="">
-                <strong>Cartoes Amarelo</strong>
-              </div>
-              <div className="">-</div>
-            </li>
-
-            <li className="flex items-center justify-between border-b border-zinc-700 px-4 py-4">
-              <div className="">
-                <span>-</span>
-              </div>
-              <div className="">
-                <strong>Cartoes Vermelho</strong>
-              </div>
-              <div className="">-</div>
-            </li>
-
-            <li className="flex items-center justify-between border-b border-zinc-700 px-4 py-4">
-              <div className="">
-                <span>-</span>
-              </div>
-              <div className="">
-                <strong>Expulso</strong>
-              </div>
-              <div className="">-</div>
-            </li>
-          </ul>
         </div>
       </div>
     </div>
--- src/components/time-card.tsx
+++ src/components/time-card.tsx
@@ -6,9 +6,10 @@ type TimeCardProps = {
   name?: string
   image: StaticImageData | string
   size?: number
+  goals?: number
 }
 
-const TimeCard: FC<TimeCardProps> = ({ image, name, size = 176 }) => {
+const TimeCard: FC<TimeCardProps> = ({ image, name, size = 176, goals }) => {
   return (
     <div className="w-full flex items-center justify-center flex-col gap-3">
       <div className="min-w-[176px] flex items-center justify-center">
@@ -19,6 +20,11 @@ const TimeCard: FC<TimeCardProps> = ({ image, name, size = 176 }) => {
           {name}
         </strong>
       </div>
+      {goals && (
+        <span className="flex rounded-lg items-center justify-center text-3xl bg-zinc-900 w-12 h-12">
+          {goals}
+        </span>
+      )}
     </div>
   )
 }
