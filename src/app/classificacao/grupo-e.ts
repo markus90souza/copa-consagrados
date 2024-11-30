2a8f66d515fbb16d6eeb71bf43c2edd45cfae6b4
rockske8er <rockske8er@gmail.com>
2024-11-30 10:42:36 -0300

    feat: fase01-quinta

---
+++ src/app/classificacao/grupo-e.ts
@@ -0,0 +1,30 @@
+export const grupoA = [
+  {
+    id: 1,
+    name: 'Doces Souza',
+    image: '/times/doces-souza.png',
+    pts: 0,
+    j: 0,
+  },
+  {
+    id: 2,
+    name: 'Galácticos',
+    image: '/times/galacticos.png',
+    pts: 0,
+    j: 0,
+  },
+  {
+    id: 3,
+    name: 'Morro da Providência',
+    image: '/times/morro.png',
+    pts: 0,
+    j: 0,
+  },
+  {
+    id: 4,
+    name: 'PSG Pituba',
+    image: '/times/psg-pituba.png',
+    pts: 0,
+    j: 0,
+  },
+]
--- src/app/classificacao/page.tsx
+++ src/app/classificacao/page.tsx
@@ -6,50 +6,62 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
-import { artilheiro } from '@/data/artilheiro'
+
 import Image from 'next/image'
 import React from 'react'
 import { ScrollArea } from '@/components/ui/scroll-area'
+import { grupoA } from './grupo-a'
 
 const ClassifacaoPage = () => {
   return (
     <Table>
       <ScrollArea className="h-fit pb-14">
         <TableHeader>
-          <TableHead className="w-10">POS</TableHead>
-          <TableHead className="w-[100px]">Jogador</TableHead>
+          <TableHead className="w-10">PTS</TableHead>
           <TableHead className="">Time</TableHead>
-          <TableHead className="w-10">Gols</TableHead>
+          <TableHead className="w-10">J</TableHead>
+          <TableHead className="w-10">V</TableHead>
+          <TableHead className="w-10">E</TableHead>
+          <TableHead className="w-10">D</TableHead>
+          <TableHead className="w-10">GF</TableHead>
+          <TableHead className="w-10">GS</TableHead>
         </TableHeader>
         <TableBody>
-          {artilheiro
-            .sort((a, b) => a.pos - b.pos)
+          {grupoA
+            .sort((a, b) => a.id - b.id)
             .map((item, index) => (
               <TableRow key={index}>
                 <TableCell className="font-mono text-sm text-center font-medium">
-                  {item.pos}
-                </TableCell>
-
-                <TableCell className="text-muted-foreground uppercase text-xs">
-                  {item.name}
+                  {item.pts}
                 </TableCell>
 
                 <TableCell>
                   <div className="flex items-center gap-2">
                     <div className="w-10">
-                      <Image
-                        src={item.shield}
-                        alt=""
-                        width={100}
-                        height={100}
-                      />
+                      <Image src={item.image} alt="" width={100} height={100} />
                     </div>
-                    <span className="text-xs uppercase">{item.time}</span>
+                    <span className="text-xs uppercase">{item.name}</span>
                   </div>
                 </TableCell>
 
+                <TableCell className="font-medium w-10 text-center">
+                  {item.j}
+                </TableCell>
+                <TableCell className="font-medium w-10 text-center">
+                  {item.v}
+                </TableCell>
+
+                <TableCell className="font-medium w-10 text-center">
+                  {item.e}
+                </TableCell>
                 <TableCell className="font-medium text-center">
-                  {item.goals}
+                  {item.d}
+                </TableCell>
+                <TableCell className="font-medium text-center">
+                  {item.gf}
+                </TableCell>
+                <TableCell className="font-medium text-center">
+                  {item.gc}
                 </TableCell>
               </TableRow>
             ))}
